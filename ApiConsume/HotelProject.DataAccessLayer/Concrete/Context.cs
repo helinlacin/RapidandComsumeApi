1ada3c2e6076f220c9359b21535c02de717c93d1
helinlacin <114211908+helinlacin@users.noreply.github.com>
2023-04-07 18:59:30 +0300

    migration oluşturuldu.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer("server=LAPTOP-SK8PA5KJ ; database=DBHotelAPİ ;Encrypt=False; User ID=sa;Password=1234; Integrated Security=True");
+
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
