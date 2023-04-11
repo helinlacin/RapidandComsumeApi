f83e5a5539f7815149ab66ac013907547a7ee496
helinlacin <114211908+helinlacin@users.noreply.github.com>
2023-04-11 16:07:52 +0300

    partials were created and edited.

--- Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -12,5 +12,29 @@ namespace HotelProject.WebUI.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult PreloaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavHeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
     }
 }
